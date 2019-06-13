5710c7acb854bb993e2ef46f24cb3cde04cb3c3e
hevzy86 <hevzy86@gmail.com>
2019-06-12 22:26:57 -0400

    installing bootstrap template

--- nodeapp.js
+++ nodeapp.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.get('/',function(req,res){
+    res.send('aloha');
+})
+
+app.get('/routing',function(req,res){
+    res.send('aloha routing');
+})
 app.listen(port, function(err){
     console.log('Server is running on port '+ port);
 });
