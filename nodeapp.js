20c50db81bffe55ce14fab95a31bd6c107c287c8
hevzy86 <hevzy86@gmail.com>
2019-06-15 01:18:37 -0400

    bower installation

--- nodeapp.js
+++ nodeapp.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static('public'));
+app.use(express.static('scr/views'));
+
+
 app.get('/',function(req,res){
     res.send('aloha');
 })
